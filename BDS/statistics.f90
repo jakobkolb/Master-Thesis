e1bfd11c6b8720c9df18f4ba352c9657ec7663f3
Jakob Kolb <jkolb@physik.hu-berlin.de>
2013-10-15 19:14:46 +0200

    changed dens profile to position histogram

--- BDS/statistics.f90
+++ BDS/statistics.f90
@@ -55,30 +55,22 @@ CONTAINS
     INTEGER :: i, imax, binnumber
     REAL    :: r, vbin
     REAL(8), DIMENSION(3)   :: POS
-    INTEGER, DIMENSION(bins)  :: hist
     REAL(8), DIMENSION(2,bins), INTENT(out) :: Xhist
     REAL(8), DIMENSION(:,:), INTENT(in)  :: X
 
-    hist = 0
     Xhist = 0
     imax = SIZE(X,2)
     POS = L/2
 
-    !$OMP PARALLEL DO REDUCTION(+:hist) PRIVATE(binnumber, r)
+    !$OMP PARALLEL DO REDUCTION(+:Xhist) PRIVATE(binnumber, r)
     DO i = 1,imax
         r = SQRT(DOT_PRODUCT(X(:,i)-POS,X(:,i)-POS))
         binnumber = INT(r/(L/SQRT(2.))*bins)
         IF(binnumber < bins) THEN
-            hist(binnumber) = hist(binnumber) + 1
+            Xhist(2,binnumber) = Xhist(2,binnumber) + 1
         ENDIF
     ENDDO
     !$OMP END PARALLEL DO
-
-    DO i = 1,bins
-        vbin = 4/3*pi*((REAL(i+1)/REAL(bins)*L/SQRT(2.))**3 - (REAL(i)/REAL(bins)*L/SQRT(2.))**3)
-        Xhist(1,i) = (REAL(i))/REAL(bins)*L/SQRT(2.)
-        Xhist(2,i) = REAL(hist(i))/vbin
-    ENDDO
 
     END SUBROUTINE histogramm
 
