8cea11e08f303f9d553bfeaf1992ad37d7ce596e
jakobkolb <jkolb@physik.hu-berlin.de>
2014-03-04 10:01:00 +0100

    absorption rate plot for poster/PRL

---
+++ Thesis/Poster/absorption_rate/rateplot.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as mp
+
+rate = np.loadtxt("rate.tsv")
+srate = np.loadtxt("srate.tsv")
+lrate = np.loadtxt("lrate.tsv")
+
+
+fig = mp.figure()
+ax1 = fig.add_subplot(111)
+ln1 = mp.plot(rate[:,0], rate[:,1], label = 'analytic solution')
+ln2 = mp.plot(srate[:,0], srate[:,1], '--', label = r' approx. for $r_d \gg 1$' )
+ln3 = mp.plot(lrate[:,0], lrate[:,1], '-.', label = r' approx. for $r_d \ll 1$' )
+ax1.set_xscale('log')
+ax1.legend(loc = 'upper right')
+ax1.set_ylabel(r'$ \frac{K}{K_{Debye}}$', fontsize = 16)
+ax1.set_xlabel(r'$r_d$', fontsize = 16)
+
+mp.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
