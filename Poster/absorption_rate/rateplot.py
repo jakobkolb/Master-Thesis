7bfeb701fe78f4d0c207aeafdffb43444e576007
Jakob Kolb <jakob.kolb@helmholtz-berlin.de>
2014-03-11 18:58:07 +0100

    updated plots for Poster and PRL

--- Poster/absorption_rate/rateplot.py
+++ Poster/absorption_rate/rateplot.py
@@ -3,15 +3,30 @@
 import numpy as np
 import matplotlib.pyplot as mp
 
-rate = np.loadtxt("rate.tsv")
-srate1 = np.loadtxt("srate1.tsv")
-srate2 = np.loadtxt("srate2.tsv")
-srate3 = np.loadtxt("srate3.tsv")
-lrate1 = np.loadtxt("lrate1.tsv")
-lrate2 = np.loadtxt("lrate2.tsv")
-lrate3 = np.loadtxt("lrate3.tsv")
+arate2  = np.loadtxt("arate2.tsv")
+arate4  = np.loadtxt("arate5.tsv")
+arate10  = np.loadtxt("arate10.tsv")
+asrate2 = np.loadtxt("asrate2.tsv")
+asrate4 = np.loadtxt("asrate5.tsv")
+asrate10 = np.loadtxt("asrate10.tsv")
+alrate2 = np.loadtxt("alrate2.tsv")
+alrate4 = np.loadtxt("alrate5.tsv")
+alrate10 = np.loadtxt("alrate10.tsv")
 
-#Direct input 
+rrate2  = np.loadtxt("rrate2.tsv")
+rrate4  = np.loadtxt("rrate5.tsv")
+rrate10  = np.loadtxt("rrate10.tsv")
+rsrate2 = np.loadtxt("rsrate2.tsv")
+rsrate4 = np.loadtxt("rsrate5.tsv")
+rsrate10 = np.loadtxt("rsrate10.tsv")
+rlrate2 = np.loadtxt("rlrate2.tsv")
+rlrate4 = np.loadtxt("rlrate5.tsv")
+rlrate10 = np.loadtxt("rlrate10.tsv")
+
+statepoints = [[1./0.04,1./0.4,1./4],[1.09836,2.13511,1.79642]]
+print statepoints[1]
+
+#Direct input
 mp.rcParams['text.latex.preamble']=[r"\usepackage{lmodern}"]
 #Options
 params = {'text.usetex' : True,
@@ -20,47 +35,62 @@ params = {'text.usetex' : True,
           'font.family' : 'lmodern',
           'text.latex.unicode': True,
           }
-mp.rcParams.update(params) 
+mp.rcParams.update(params)
 
 fig = mp.figure()
 
 #You must select the correct size of the plot in advance
-fig.set_size_inches(3.54,3.84) 
+fig.set_size_inches(3.54,2.*3.84)
 
-ax1 = fig.add_subplot(111)
-ln1a = mp.plot(rate[:,0], rate[:,1], color = '0.55', label = 'analytic solution')
-ln1b = mp.plot(rate[:,0], rate[:,2], color = '0.55')
-ln1c = mp.plot(rate[:,0], rate[:,3], color = '0.55')
-ln2a = mp.plot(srate1[:,0], srate1[:,1], 'r--', label = r' approx. for $r_d \gg 1$' )
-ln2b = mp.plot(srate2[:,0], srate2[:,1], 'r--')
-ln2c = mp.plot(srate3[:,0], srate3[:,1], 'r--')
-ln3a = mp.plot(lrate1[:,0], lrate1[:,1], 'b-.', label = r' approx. for $r_d \ll 1$' )
-ln3b = mp.plot(lrate2[:,0], lrate2[:,1], 'b-.')
-ln3c = mp.plot(lrate3[:,0], lrate3[:,1], 'b-.')
+ax1 = fig.add_subplot(211)
+ln1a = mp.plot(arate2[:,0],     arate2[:,1], color = '0.55', label = 'analytic solution')
+ln1b = mp.plot(arate4[:,0],     arate4[:,1], color = '0.55')
+ln1c = mp.plot(arate10[:,0],    arate10[:,1], color = '0.55')
+ln2a = mp.plot(asrate2[:,0],    asrate2[:,1], 'r--', label = r' approx. for $r_d \gg 1$' )
+ln2b = mp.plot(asrate4[:,0],    asrate4[:,1], 'r--')
+ln2c = mp.plot(asrate10[:,0],   asrate10[:,1], 'r--')
+ln3a = mp.plot(alrate2[:,0],    alrate2[:,1], 'b-.', label = r' approx. for $r_d \ll 1$' )
+ln3b = mp.plot(alrate4[:,0],    alrate4[:,1], 'b-.')
+ln3c = mp.plot(alrate10[:,0],   alrate10[:,1], 'b-.')
+ax1.set_xscale('log')
+ax1.set_ylim([0.98,1.4])
+ax1.set_yticks(np.arange(1.,1.5,0.1))
+ax1.set_ylabel(r'$ K/K_{Debye}$')
+ax1.set_xlabel(r'$r_d$')
+ax1.annotate('increasing t', xy=(10**0.5, 1.05),  xycoords='data',
+                xytext=(40, 60), textcoords='offset points',
+                arrowprops=dict(arrowstyle="<-")
+                )
 
-ax1.annotate('increasing t', xy=(10**0, 0.7),  xycoords='data',
+
+ax2 = fig.add_subplot(212)
+mp.plot(statepoints[0], statepoints[1], 'kx', label = 'state points')
+ln1a = mp.plot(rrate2[:,0],     rrate2[:,1], color = '0.55', label = 'analytic solution')
+ln1b = mp.plot(rrate4[:,0],     rrate4[:,1], color = '0.55')
+ln1c = mp.plot(rrate10[:,0],    rrate10[:,1], color = '0.55')
+ln2a = mp.plot(rsrate2[:,0],    rsrate2[:,1], 'r--', label = r' approx. for $r_d \gg 1$' )
+ln2b = mp.plot(rsrate4[:,0],    rsrate4[:,1], 'r--')
+ln2c = mp.plot(rsrate10[:,0],   rsrate10[:,1], 'r--')
+ln3a = mp.plot(rlrate2[:,0],    rlrate2[:,1], 'b-.', label = r' approx. for $r_d \ll 1$' )
+ln3b = mp.plot(rlrate4[:,0],    rlrate4[:,1], 'b-.')
+ln3c = mp.plot(rlrate10[:,0],   rlrate10[:,1], 'b-.')
+ax2.set_xscale('log')
+ax2.set_ylim([0.9,3.5])
+ax2.legend(loc = 'upper right')
+ax2.set_ylabel(r'$ K/K_{Debye}$')
+ax2.set_xlabel(r'$r_d$')
+ax2.annotate('increasing t', xy=(10**0, 1.4),  xycoords='data',
                 xytext=(60, 60), textcoords='offset points',
                 arrowprops=dict(arrowstyle="<-")
                 )
 
-#ax1.annotate('increasing t', xy=(10**0.5, 1.15),  xycoords='data',
-#                xytext=(40, 60), textcoords='offset points',
-#                arrowprops=dict(arrowstyle="<-")
-#                )
 
 
-ax1.set_xscale('log')
-ax1.set_ylim([0.4,1.6])
-#ax1.set_ylim([1,1.5])
-ax1.legend(loc = 'upper right')
-ax1.set_ylabel(r'$ K/K_{Debye}$')
-ax1.set_xlabel(r'$r_d$')
-
-mp.savefig("density_profiles.pdf", 
+mp.savefig("density_profiles.pdf",
             #This is simple recomendation for publication plots
-            dpi=1000, 
+            dpi=1000,
             # Plot will be occupy a maximum of available space
-            bbox_inches='tight', 
+            bbox_inches='tight',
             )
 
 
--- Poster/resonant_switching_rate/plotmax.py
+++ Poster/resonant_switching_rate/plotmax.py
@@ -28,9 +28,9 @@ def calc_rate(u,rate,spacing,kt,d):
     s[0:2,1] = s[0:2,1]/np.linalg.norm(s[0:2,1])
 
     invs = np.linalg.inv(s)
-    
+
     k=(-np.exp(4*a*x)+np.exp(2*(1+a)*x)-np.exp(2*(1+b)*x)+np.exp(2*(a+b)*x)+a*np.exp(4*a*x)*x-b*np.exp(4*a*x)*x-a*np.exp(2*(1+a)*x)*x+b*np.exp(2*(1+a)*x)*x-a*np.exp(2*(1+b)*x)*x+3*b*np.exp(2*(1+b)*x)*x+a*np.exp(2*(a+b)*x)*x-3*b*np.exp(2*(a+b)*x)*x-2*b*np.exp((2+a+b)*x)*x+2*b*np.exp((3*a+b)*x)*x+a*b*np.exp(4*a*x)*x*x-a*b*np.exp(2*(1+a)*x)*x*x+3*a*b*np.exp(2*(1+b)*x)*x*x-3*a*b*np.exp(2*(a+b)*x)*x*x-2*b*b*np.exp((2+a+b)*x)*x*x+2*b*b*np.exp((3*a+b)*x)*x*x)/(-np.exp(4*a*x)+np.exp(2*(1+a)*x)-np.exp(2*(1+b)*x)+np.exp(2*(a+b)*x)+a*np.exp(4*a*x)*x-b*np.exp(4*a*x)*x+2*np.exp(2*(1+a)*x)*x-3*a*np.exp(2*(1+a)*x)*x+b*np.exp(2*(1+a)*x)*x+2*np.exp(2*(1+b)*x)*x-a*np.exp(2*(1+b)*x)*x+b*np.exp(2*(1+b)*x)*x-4*np.exp(2*(a+b)*x)*x+3*a*np.exp(2*(a+b)*x)*x-b*np.exp(2*(a+b)*x)*x-4*np.exp((2+a+b)*x)*x+2*a*np.exp((2+a+b)*x)*x+4*np.exp((3*a+b)*x)*x-2*a*np.exp((3*a+b)*x)*x+a*b*np.exp(4*a*x)*x*x+2*b*np.exp(2*(1+a)*x)*x*x-3*a*b*np.exp(2*(1+a)*x)*x*x+2*a*np.exp(2*(1+b)*x)*x*x+a*b*np.exp(2*(1+b)*x)*x*x-2*a*np.exp(2*(a+b)*x)*x*x+2*b*np.exp(2*(a+b)*x)*x*x-3*a*b*np.exp(2*(a+b)*x)*x*x-2*a*np.exp((2+a+b)*x)*x*x+2*a*a*np.exp((2+a+b)*x)*x*x-2*b*np.exp((2+a+b)*x)*x*x-2*a*np.exp((3*a+b)*x)*x*x+2*a*a*np.exp((3*a+b)*x)*x*x+2*b*np.exp((3*a+b)*x)*x*x)
-    
+
     return k
 
 def powerlaw(x,a,b):
@@ -39,10 +39,11 @@ def powerlaw(x,a,b):
 u = 10.
 
 srates = 10**np.arange(-1,6,0.001)
-tvalues = 10**np.arange(-1,0.2,0.1)
+tvalues = 10**np.arange(-1,100,1)
 #gvalues = np.arange(1,4,0.1)
-gvalues = [1,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.81,1.9,2.,2.11,2.2,2.31,2.4,2.5,2.6,2.7,2.8,2.9,3.]
+#gvalues = [1,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.81,1.9,2.,2.11,2.2,2.31,2.4,2.5,2.6,2.7,2.8,2.9,3.]
 #gvalues = [1,2,3]
+gvalues = [1]
 arates = np.zeros((np.shape(srates)[0],2))
 kmax = np.zeros((np.shape(tvalues)[0],np.shape(gvalues)[0],4))
 fit_parameters = np.zeros((np.shape(gvalues)[0],3))
@@ -71,7 +72,7 @@ for g in gvalues:
     print cov, g
     fit_parameters[g_index,:] = g, popt[0], popt[1]
 
-#Direct input 
+#Direct input
 mp.rcParams['text.latex.preamble']=[r"\usepackage{lmodern}"]
 #Options
 params = {'text.usetex' : True,
@@ -79,7 +80,7 @@ params = {'text.usetex' : True,
           'font.family' : 'lmodern',
           'text.latex.unicode': True,
           }
-mp.rcParams.update(params) 
+mp.rcParams.update(params)
 fig1 = mp.figure()
 ax1 = fig1.add_subplot(111)
 for i in range(np.shape(gvalues)[0]):
@@ -93,14 +94,14 @@ ax1.set_xscale('log')
 ax1.set_xlim(0.1,1.1)
 
 #You must select the correct size of the plot in advance
-fig1.set_size_inches(3.54,3.54) 
+fig1.set_size_inches(3.54,3.54)
 
 
-mp.savefig("resonant_decay_length.pdf", 
+mp.savefig("resonant_decay_length.pdf",
             #This is simple recomendation for publication plots
-            dpi=1000, 
+            dpi=1000,
             # Plot will be occupy a maximum of available space
-            bbox_inches='tight', 
+            bbox_inches='tight',
             )
 
 
@@ -116,14 +117,14 @@ ax32.set_xlabel(r'$g$')
 mp.plot(fit_parameters[:,0], fit_parameters[:,2])
 
 #You must select the correct size of the plot in advance
-fig3.set_size_inches(3.54,4.54) 
+fig3.set_size_inches(3.54,4.54)
 
 
-mp.savefig("powerlaw_parameters.pdf", 
+mp.savefig("powerlaw_parameters.pdf",
             #This is simple recomendation for publication plots
-            dpi=1000, 
+            dpi=1000,
             # Plot will be occupy a maximum of available space
-            bbox_inches='tight', 
+            bbox_inches='tight',
             )
 
 
