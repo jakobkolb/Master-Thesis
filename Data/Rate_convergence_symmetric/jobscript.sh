f1b9a38f74aeb951ad6136b22a7ffda30e0bcce6
Jakob Kolb <jkolb@physik.hu-berlin.de>
2014-04-23 18:19:42 +0200

    cleaning up

---
+++ Data/Rate_convergence_symmetric/jobscript.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -login
+ 
+# Torque/PBS Job Script for MathKernel on HPCC Systems
+#
+# 2010/11/16 - Eric McDonald
+# * Create.
+ 
+#PBS -N ssc_run
+#PBS -l nodes=1:ppn=4,walltime=720:00:00
+#PBS -m ae
+ 
+cd $PBS_O_WORKDIR
+ 
+echo "Information about the job:"
+qstat -f $PBS_JOBID
+echo -e "\n\n"
+ 
+echo "Started batch processing at `date`."
+math -script ssc.m
+echo "Ended batch processing at `date`."
+ 
+exit 0
