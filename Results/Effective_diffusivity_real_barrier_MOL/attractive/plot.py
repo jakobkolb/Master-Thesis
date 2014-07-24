699daea8e19aaec794b76189d58e89f4f73fb258
jakobkolb <jkolb@physik.hu-berlin.de>
2014-07-24 10:38:34 +0200

    sync

--- Results/Effective_diffusivity_real_barrier_MOL/attractive/plot.py
+++ Results/Effective_diffusivity_real_barrier_MOL/attractive/plot.py
@@ -14,9 +14,10 @@ d_n = np.zeros((resolution,1))
 tmp = np.zeros((resolution,1))
 k = 0
 
-for i in range(4):
+for i, directory in enumerate(os.listdir('.')):
+    if 'attractive_rd_' in directory:
         k+=1
-        tmp[:,0] = np.loadtxt('att' + `i+1` + '/effective_d_profile.tsv')
+        tmp[:,0] = np.loadtxt(directory + '/effective_d_profile.tsv')
         d_n = np.append(d_n, tmp, axis=1)
 print np.shape(d_n)
 
@@ -34,19 +35,21 @@ mp.rcParams.update(params)
 fig2 = mp.figure()
 fig2.set_size_inches(3.54,2.64)
 d_ax = fig2.add_subplot(111)
+pot_ax = d_ax.twinx()
 
-l1 = d_ax.plot(r_n[2:-1], d_n[2:-1,1], 'r-', label = r'$r_d = 10^{-2}$')
-l2 = d_ax.plot(r_n[2:-1], d_n[2:-1,2], 'r:', label = r'$r_d = 10^{-1}$')
-l3 = d_ax.plot(r_n[2:-1], d_n[2:-1,3], 'r--', label = r'$r_d = 2.5$')
-l4 = d_ax.plot(r_n[2:-1], d_n[2:-1,4], 'r-.', label = r'$r_d = 100$')
+l1 = d_ax.plot(r_n[2:-1], d_n[2:-1,1], 'r:', label = r'$r_d = 20$')
+l2 = d_ax.plot(r_n[2:-1], d_n[2:-1,2], 'r-.', label = r'$r_d = 2$')
+l3 = d_ax.plot(r_n[2:-1], d_n[2:-1,3], 'r--', label = r'$r_d = 0.5$')
+l4 = pot_ax.plot(r_n, u_n, color='0.55', label = 'potential')
 d_ax.set_ylabel(r'$D_{eff}(r)$')
+pot_ax.set_ylabel(r'$U_{m}(r)$')
 d_ax.set_xlabel('r')
-#d_ax.set_yscale('log')
+d_ax.set_yscale('log')
 
 lns = l1+l2+l3+l4
 labs = [l.get_label() for l in lns]
-d_ax.legend(lns, labs, loc='lower right')
-mp.savefig("diff.pdf",
+d_ax.legend(lns, labs, loc='upper right')
+mp.savefig("diff_attractive.pdf",
         #This is simple recomendation for publication plots
         dpi=1000,
         # Plot will be occupy a maximum of available space
