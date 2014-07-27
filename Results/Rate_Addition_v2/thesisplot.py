17ea0ad2c6e2191660b86bb726ec1229c6ad03a6
jakobkolb <jkolb@physik.hu-berlin.de>
2014-07-27 13:23:09 +0200

    updated thesis

--- Results/Rate_Addition_v2/thesisplot.py
+++ Results/Rate_Addition_v2/thesisplot.py
@@ -12,45 +12,38 @@ Knum_att = {}
 Knum_rep = {}
 
 Ksvals = ['0.1','1','10']
+nKsvals = [0.1,1,10]
 
 for i, k in enumerate(Ksvals):
     print i, k
 
     Keff_tmp_att = np.loadtxt('attasumption'+Ksvals[i]+'.tsv')
-#    Keff_tmp_rep = np.loadtxt('repasumption'+Ksvals[i]+'.tsv')
+    Keff_tmp_rep = np.loadtxt('repasumption'+Ksvals[i]+'.tsv')
     Kbc_tmp_att = np.loadtxt('attanalytics'+Ksvals[i]+'.tsv')
-#    Kbc_tmp_rep = np.loadtxt('repanalytics'+Ksvals[i]+'.tsv')
+    Kbc_tmp_rep = np.loadtxt('repanalytics'+Ksvals[i]+'.tsv')
     
     Keff_att[i] = Keff_tmp_att
-#    Keff_rep[i] = Keff_tmp_rep
+    Keff_rep[i] = Keff_tmp_rep
     Kbc_att[i] = Kbc_tmp_att
-#    Kbc_rep[i] = Kbc_tmp_rep
-    
+    Kbc_rep[i] = Kbc_tmp_rep
 
-Knum_tmp_att = np.loadtxt('att_numeric_rates.csv', delimiter=',')
-#Knum_tmp_rep = np.loadtxt('rep_numeric_rates.csv', delimiter=',')
+N = np.shape(Keff_tmp_att)[0]/3
 
-print np.shape(Knum_tmp_att)
+Knum_att = np.loadtxt('att_numeric_rates.csv', delimiter=',')
+Knum_rep = np.loadtxt('rep_numeric_rates.csv', delimiter=',')
 
-N_tmp_att = np.zeros((np.shape(Knum_tmp_att)[0],2))
-#N_tmp_rep = np.zeros((np.shape(Knum_tmp_rep)[0],2))
-print np.shape(N_tmp_att)
+a=6
+b=11
+ur=3
+ua=-3
+
+Kd_rep = 0.831
+Kd_att = 1.122
 
 fig1 = mp.figure()
 ax1 = fig1.add_subplot(111)
 
-for i, k in enumerate(Ksvals):
-    N_tmp_att[:,0] = Knum_tmp_att[:,0]
-    N_tmp_att[:,1] = Knum_tmp_att[:,i+1]
-#    N_tmp_rep[:,0] = Knum_tmp_rep[:,0]
-#    N_tmp_rep[:,1] = Knum_tmp_rep[:,i+1]
-    Knum_att[i] = N_tmp_att
-#    Knum_rep[i] = N_tmp_rep
-    ax1.plot(Knum_att[i][:,0],Knum_att[i][:,1], 'o', label = r'$K_s = $'+k)
-
-ax1.set_xscale('log')
-
-mp.legend()
+j = 1
 
 #Direct input 
 mp.rcParams['text.latex.preamble']=[r"\usepackage{lmodern, MnSymbol}"]
@@ -64,65 +57,198 @@ params = {'text.usetex' : True,
 mp.rcParams.update(params) 
 height = 0.8
 
+if False:
+    fig1 = mp.figure()
+    ax1 = fig1.add_subplot(111)
+    fig1.set_size_inches(3.54,height*3.54) 
 
-fig1 = mp.figure()
-ax1 = fig1.add_subplot(111)
-fig1.set_size_inches(3.54,height*3.54) 
+    i = 0
 
-i = 0
+    Ks = nKsvals[i]
+    Keff = Ks*Kd_att/(Ks+Kd_att)
 
-ax1.plot(Keff_att[i][:,0],Keff_att[i][:,1], '-',color = reds[i], label = r'$K_{eff}$')
-ax1.plot(Kbc_att[i][:,0],Kbc_att[i][:,1], '-.', color = reds[i], label = r'$K_{bc}$')    
-ax1.plot(Knum_att[i][:,0],Knum_att[i][:,1], 'o',color = blues[i], label = r'$K_N$')
-ax1.set_xscale('log')
-mp.legend()
+    print Keff
 
-mp.savefig('rate_comparison'+`i`+'.pdf', 
-            #This is simple recomendation for publication plots
-            dpi=1000, 
-            # Plot will be occupy a maximum of available space
-            bbox_inches='tight', 
-            pad_inches=0.02
-            )
+    ax1.text(0.02, 0.95, '$K_A = $'+Ksvals[i], transform=ax1.transAxes, fontsize=12, va='top')
+    ax1.plot(Keff_att[i][:,0],Keff_att[i][:,1], '-',color = reds[j], label = r'$K_{eff}$')
+    ax1.plot(Kbc_att[i][:,0],Kbc_att[i][:,1], '-.', color = reds[j], label = r'$K_{bc}$')    
+    ax1.plot(Kbc_att[i][0:N,0],Keff*np.ones((N)), ':', color = blues[j], label = r'$K_{m}$')    
+    ax1.plot(Knum_att[:,0],Knum_att[:,1+i], 'o',color = blues[j], label = r'$K_N$')
+    ax1.set_xscale('log')
+    mp.legend()
 
+    ax1.set_xlabel('$r_d$')
+    ax1.set_ylabel('$K/K_S$')
 
-fig2 = mp.figure()
-ax2 = fig2.add_subplot(111)
-fig2.set_size_inches(3.54,height*3.54) 
-
-i = 1
-
-ax2.plot(Keff_att[i][:,0],Keff_att[i][:,1], '-',color = reds[i], label = r'$K_{eff}$')
-ax2.plot(Kbc_att[i][:,0],Kbc_att[i][:,1], '-.', color = reds[i], label = r'$K_{bc}$')   
-ax2.plot(Knum_att[i][:,0],Knum_att[i][:,1], 'o',color = blues[i],  label = r'$K_N$')
-ax2.set_xscale('log')
-mp.legend()
-
-mp.savefig('rate_comparison'+`i`+'.pdf', 
-            #This is simple recomendation for publication plots
-            dpi=1000, 
-            # Plot will be occupy a maximum of available space
-            bbox_inches='tight', 
-            pad_inches=0.02
-            )
+    mp.savefig('att_rate_comparison'+`i`+'.pdf', 
+                #This is simple recomendation for publication plots
+                dpi=1000, 
+                # Plot will be occupy a maximum of available space
+                bbox_inches='tight', 
+                pad_inches=0.02
+                )
 
 
-fig3 = mp.figure()
-ax3 = fig3.add_subplot(111)
-fig3.set_size_inches(3.54,height*3.54) 
+    fig2 = mp.figure()
+    ax2 = fig2.add_subplot(111)
+    fig2.set_size_inches(3.54,height*3.54) 
 
-i = 2
+    i = 1
 
-ax3.plot(Keff_att[i][:,0],Keff_att[i][:,1], '-',color = reds[i], label = r'$K_{eff}$')
-ax3.plot(Kbc_att[i][:,0],Kbc_att[i][:,1], '-.', color = reds[i], label = r'$K_{bc}$')   
-ax3.plot(Knum_att[i][:,0],Knum_att[i][:,1], 'o',color = blues[i], label = r'$K_N$')
-ax3.set_xscale('log')
-mp.legend()
+    Ks = nKsvals[i]
+    Keff = Ks*Kd_att/(Ks+Kd_att)
 
-mp.savefig('rate_comparison'+`i`+'.pdf', 
-            #This is simple recomendation for publication plots
-            dpi=1000, 
-            # Plot will be occupy a maximum of available space
-            bbox_inches='tight', 
-            pad_inches=0.02
-            )
+    print Keff
+
+    ax2.text(0.02, 0.95, '$K_A = $'+Ksvals[i], transform=ax2.transAxes, fontsize=12, va='top')
+    ax2.plot(Keff_att[i][:,0],Keff_att[i][:,1], '-',color = reds[j], label = r'$K_{eff}$')
+    ax2.plot(Kbc_att[i][:,0],Kbc_att[i][:,1], '-.', color = reds[j], label = r'$K_{bc}$')   
+    ax2.plot(Kbc_att[i][0:N,0],Keff*np.ones((N)), ':', color = blues[j], label = r'$K_{m}$')    
+    ax2.plot(Knum_att[:,0],Knum_att[:,1+i], 'o',color = blues[j],  label = r'$K_N$')
+    ax2.set_xscale('log')
+    mp.legend()
+
+    ax2.set_xlabel('$r_d$')
+    ax2.set_ylabel('$K/K_S$')
+
+    mp.savefig('att_rate_comparison'+`i`+'.pdf', 
+                #This is simple recomendation for publication plots
+                dpi=1000, 
+                # Plot will be occupy a maximum of available space
+                bbox_inches='tight', 
+                pad_inches=0.02
+                )
+
+
+    fig3 = mp.figure()
+    ax3 = fig3.add_subplot(111)
+    fig3.set_size_inches(3.54,height*3.54) 
+
+    i = 2
+
+    Ks = nKsvals[i]
+    Keff = Ks*Kd_att/(Ks+Kd_att)
+
+    print Keff
+
+    ax3.text(0.02, 0.95, '$K_A = $'+Ksvals[i], transform=ax3.transAxes, fontsize=12, va='top')
+    ax3.plot(Keff_att[i][:,0],Keff_att[i][:,1], '-',color = reds[j], label = r'$K_{eff}$')
+    ax3.plot(Kbc_att[i][:,0],Kbc_att[i][:,1], '-.', color = reds[j], label = r'$K_{bc}$')   
+    ax3.plot(Kbc_att[i][0:N,0],Keff*np.ones((N)), ':', color = blues[j], label = r'$K_{m}$')    
+    ax3.plot(Knum_att[:,0],Knum_att[:,1+i], 'o',color = blues[j], label = r'$K_N$')
+    ax3.set_xscale('log')
+    mp.legend()
+
+    ax3.set_xlabel('$r_d$')
+    ax3.set_ylabel('$K/K_S$')
+
+    mp.savefig('att_rate_comparison'+`i`+'.pdf', 
+                #This is simple recomendation for publication plots
+                dpi=1000, 
+                # Plot will be occupy a maximum of available space
+                bbox_inches='tight', 
+                pad_inches=0.02
+                )
+
+if True:
+
+    fig1 = mp.figure()
+    ax1 = fig1.add_subplot(111)
+    fig1.set_size_inches(3.54,height*3.54) 
+
+    i = 0
+
+    Ks = nKsvals[i]
+    Keff = Ks*Kd_rep/(Ks+Kd_rep)
+
+    print Keff
+
+    ax1.text(0.02, 0.95, '$K_A = $'+Ksvals[i], transform=ax1.transAxes, fontsize=12, va='top')
+    ax1.plot(Keff_rep[i][:,0],Keff_rep[i][:,1], '-',color = reds[j], label = r'$K_{eff}$')
+    ax1.plot(Kbc_rep[i][:,0],Kbc_rep[i][:,1], '-.', color = reds[j], label = r'$K_{bc}$')    
+    ax1.plot(Kbc_att[i][0:N,0],Keff*np.ones((N)), ':', color = blues[j], label = r'$K_{m}$')    
+    ax1.plot(Knum_rep[:,0],Knum_rep[:,1+i], 'o',color = blues[j], label = r'$K_N$')
+    ax1.set_xscale('log')
+    mp.legend()
+
+    ax1.set_xlabel('$r_d$')
+    ax1.set_ylabel('$K/K_S$')
+
+    ax1.set_ylim([0.08,0.12])
+    ax1.set_yticks([0.08,0.09,0.10,0.11,0.12])
+
+    mp.savefig('rep_rate_comparison'+`i`+'.pdf', 
+                #This is simple recomendation for publication plots
+                dpi=1000, 
+                # Plot will be occupy a maximum of available space
+                bbox_inches='tight', 
+                pad_inches=0.02
+                )
+
+
+    fig2 = mp.figure()
+    ax2 = fig2.add_subplot(111)
+    fig2.set_size_inches(3.54,height*3.54) 
+
+    i = 1
+
+    Ks = nKsvals[i]
+    Keff = Ks*Kd_rep/(Ks+Kd_rep)
+
+    print Keff
+
+    ax2.text(0.02, 0.95, '$K_A = $'+Ksvals[i], transform=ax2.transAxes, fontsize=12, va='top')
+    ax2.plot(Keff_rep[i][:,0],Keff_rep[i][:,1], '-',color = reds[j], label = r'$K_{eff}$')
+    ax2.plot(Kbc_rep[i][:,0],Kbc_rep[i][:,1], '-.', color = reds[j], label = r'$K_{bc}$')   
+    ax2.plot(Kbc_att[i][0:N,0],Keff*np.ones((N)), ':', color = blues[j], label = r'$K_{m}$')    
+    ax2.plot(Knum_rep[:,0],Knum_rep[:,1+i], 'o',color = blues[j],  label = r'$K_N$')
+    ax2.set_xscale('log')
+    mp.legend()
+
+    ax2.set_xlabel('$r_d$')
+    ax2.set_ylabel('$K/K_S$')
+
+    ax2.set_ylim([0.37,0.62])
+    ax2.set_yticks([0.40,0.45,0.50,0.55,0.60])
+
+    mp.savefig('rep_rate_comparison'+`i`+'.pdf', 
+                #This is simple recomendation for publication plots
+                dpi=1000, 
+                # Plot will be occupy a maximum of available space
+                bbox_inches='tight', 
+                pad_inches=0.02
+                )
+
+
+    fig3 = mp.figure()
+    ax3 = fig3.add_subplot(111)
+    fig3.set_size_inches(3.54,height*3.54) 
+
+    i = 2
+
+    Ks = nKsvals[i]
+    Keff = Ks*Kd_rep/(Ks+Kd_rep)
+
+    print Keff
+
+    ax3.text(0.02, 0.95, '$K_A = $'+Ksvals[i], transform=ax3.transAxes, fontsize=12, va='top')
+    ax3.plot(Keff_rep[i][:,0],Keff_rep[i][:,1], '-',color = reds[j], label = r'$K_{eff}$')
+    ax3.plot(Kbc_rep[i][:,0],Kbc_rep[i][:,1], '-.', color = reds[j], label = r'$K_{bc}$')   
+    ax3.plot(Kbc_att[i][0:N,0],Keff*np.ones((N)), ':', color = blues[j], label = r'$K_{m}$')    
+    ax3.plot(Knum_rep[:,0],Knum_rep[:,1+i], 'o',color = blues[j], label = r'$K_N$')
+    ax3.set_xscale('log')
+    mp.legend()
+
+    ax3.set_xlabel('$r_d$')
+    ax3.set_ylabel('$K/K_S$')
+
+    ax3.set_ylim([0.6,1.05])
+    ax3.set_yticks([0.65,0.75,0.85,0.95])
+
+    mp.savefig('rep_rate_comparison'+`i`+'.pdf', 
+                #This is simple recomendation for publication plots
+                dpi=1000, 
+                # Plot will be occupy a maximum of available space
+                bbox_inches='tight', 
+                pad_inches=0.02
+                )
