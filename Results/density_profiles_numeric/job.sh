5b228f029bbfe434e7b20be69677b8fe42fc9ba9
Jakob Kolb <jkolb@physik.hu-berlin.de>
2014-07-30 12:59:59 +0200

    density profiles numeric added

--- Results/density_profiles_numeric/job.sh
+++ Results/density_profiles_numeric/job.sh
@@ -9,18 +9,11 @@ cd $PBS_O_WORKDIR
 echo "Information about the job:"
 qstat -f $PBS_JOBID
 echo -e "\n\n"
-
-rm att_rd_05.*
-rm *.tsv
-rm *.out
-rm *.pdf
-rm done.txt
  
 echo "Started batch processing at `date`."
 math -script numeric_integration.m
 echo "mathematica finished at `date`."
-python plot0.py
-
-echo 'done'>done.txt
-
+python 2_Eff.py
+echo "Ended batch processing at `date`."
+ 
 exit 0
---
+++ Results/density_profiles_numeric/rep250/numeric_integration.m
@@ -0,0 +1,63 @@
+Clear["Global`*"]
+SetSystemOptions["CatchMachineUnderflow" -> False]
+<<input.py
+
+Print[U01, U02, a, b, n, Rsink, Rmax, rd]
+
+a = Rsink+l;
+b = Rsink+l+l*g
+
+at = a + (b-a)/2
+bt = (b-a)/2;
+
+Print[a]
+Print[b]
+Print[at]
+Print[bt]
+
+npoints = nmax - nmin;
+
+u1[r_] := d U01 Exp[-((r - at)/bt)^n];
+u2[r_] := d U02 Exp[-((r - at)/bt)^n];
+pde = {D[\[Rho]1[r, t], t] == 
+    2/r \[Rho]1[r, t] u1'[r] + 
+     D[\[Rho]1[r, t], r] u1'[r] + \[Rho]1[r, t] u1''[r] + 
+     d 2/r D[\[Rho]1[r, t], r] + d D[\[Rho]1[r, t], r, r] - 
+     w12 \[Rho]1[r, t] + w21 \[Rho]2[r, t],
+   D[\[Rho]2[r, t], t] == 
+    2/r \[Rho]2[r, t] u2'[r] + 
+     D[\[Rho]2[r, t], r] u2'[r] + \[Rho]2[r, t] u2''[r] + 
+     d 2/r D[\[Rho]2[r, t], r] + d D[\[Rho]2[r, t], r, r] - 
+     w21 \[Rho]2[r, t] + w12 \[Rho]1[r, t]
+   };
+bc = {
+   \[Rho]1[Rsink, t] == 0,
+   \[Rho]2[Rsink, t] == 0,
+   \[Rho]1[Rmax, t] == w21/(w12 + w21),
+   \[Rho]2[Rmax, t] == w12/(w12 + w21)
+   };
+ic = {
+   \[Rho]1[r, t0] == w21/(w12 + w21)*Exp[-((r - Rmax)/(Rmax/4))^n],
+   \[Rho]2[r, t0] == w12/(w12 + w21)*Exp[-((r - Rmax)/(Rmax/4))^n]
+   };
+
+
+w21 = d/(2*rd^2);
+w12 = w21;
+sol = NDSolve[{pde, bc, ic}, {\[Rho]1, \[Rho]2}, {t, t1, t1}, {r, 
+    Rsink, Rmax}, MaxSteps -> Infinity, MaxStepFraction -> 0.002, 
+   AccuracyGoal -> 15, StartingStepSize -> 0.001, 
+   Method -> {"MethodOfLines", 
+     "SpatialDiscretization" -> {"TensorProductGrid", 
+       "MinPoints" -> 24000}}];
+\[Rho]1eq[r_] = \[Rho]1[r, t1] /. sol;
+\[Rho]2eq[r_] = \[Rho]2[r, t1] /. sol;
+\[Rho]tot[r_] = (\[Rho]1[r, t1] + \[Rho]2[r, t1]) /. sol;
+
+rhovals = 
+ Partition[
+  Flatten[Table[N[{r, \[Rho]1eq[r], \[Rho]2eq[r] , \[Rho]tot[r], (u1[r]+u2[r])/2}], {r, Rsink, 
+     Rmax, (Rmax - Rsink)/resolution}]], 5]
+
+Export["rhovals.tsv", N[rhovals], "TSV"]
+
