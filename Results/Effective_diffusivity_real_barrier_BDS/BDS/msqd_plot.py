f806a6460752c8d8e356394c0569faf87633ef39
jakobkolb <jkolb@physik.hu-berlin.de>
2014-08-08 12:25:36 +0200

    sync

--- Results/Effective_diffusivity_real_barrier_BDS/BDS/msqd_plot.py
+++ Results/Effective_diffusivity_real_barrier_BDS/BDS/msqd_plot.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as mp
 import scipy.optimize as cp
+import math
 
 data = np.loadtxt('mean_square_radius.tsv')
 parameters = np.loadtxt('rate_data01', skiprows=2)
@@ -9,7 +10,7 @@ parameters = np.loadtxt('rate_data01', skiprows=2)
 def powerlaw(t,D,alpha):
     return 2.0*D*np.power(t,alpha)
 
-cutoff = 10
+cutoff = 20
 nbins = np.shape(data)[1]
 ntimes = np.shape(data)[0]
 dt = float(parameters[3])
@@ -19,18 +20,24 @@ fitparameters = np.zeros((nbins,2))
 
 fig = mp.figure()
 
-ax1 = fig.add_subplot(131)
-ax2 = fig.add_subplot(132)
-ax3 = fig.add_subplot(133)
+ax1 = fig.add_subplot(111)
+#ax2 = fig.add_subplot(132)
+#ax3 = fig.add_subplot(133)
+
+mdata = np.zeros(np.shape(data[:,cutoff]))
+print mdata
+for ibin in range(cutoff,nbins-cutoff):
+    for j in range(np.shape(data[:,ibin])[0]):
+        if not math.isnan(data[j,ibin]):
+            mdata[j] = mdata[j] + data[j,ibin]
+    ax1.plot(times,data[:,ibin])
+ax1.plot(times, mdata/float(nbins-2*cutoff))
 
 for ibin in range(cutoff,nbins-cutoff):
     par, cov = cp.curve_fit(powerlaw, times, data[:,ibin])
-    if par[0] != 1:
-        ax1.plot(times, data[:,ibin])
-        ax2.plot(ibin, par[0], 'o')
-        ax3.plot(ibin, par[1], 'o')
-    print ibin, par[0]
-    print ibin, par[1]
+#    if par[0] != 1:
+#    ax2.plot(ibin, par[0], 'o')
+#    ax3.plot(ibin, par[1], 'o')
 
 
 mp.show()
